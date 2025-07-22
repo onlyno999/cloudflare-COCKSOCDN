391e4b2f130a7c1f9ff9045128486191df0ecb7b
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2025-07-22 14:40:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -42,7 +42,7 @@ let 启用SOCKS5全局反代 = true; // 选择是否启用SOCKS5全局反代，�
 let 我的SOCKS5账号 = ''; // 格式'账号:密码@地址:端口'，可以通过环境变量SOCKS5_ADDRESS控制
 
 // 新增：SOCKS5 地址列表 URL
-let SOCKS5地址列表URL = 'https://raw.githubusercontent.com/onlyno999/cloudflare-COCKSOCDN/main/socks.txt'; // 可以通过环境变量 SOCKS5_TXT_URL 控制
+let SOCKS5地址列表URL = ''; // 可以通过环境变量 SOCKS5_TXT_URL 控制
 
 // SOCKS5 地址池和当前索引 (在并发模式下，索引更多用于初始加载后的顺序，实际连接由 Promise.any 管理)
 let SOCKS5地址池 = [];
